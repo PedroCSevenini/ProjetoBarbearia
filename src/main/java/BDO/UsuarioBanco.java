2a9c352d405144dcfa2883c2267de2af197c181e
Pedro Sevenini <pedro.sevenini@estudante.ufjf.br>
2024-09-08 13:24:47 -0300

    Tratamento de BDO com txt, criação UsuarioBanco e Usuario.txt

---
+++ src/main/java/BDO/UsuarioBanco.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BDO;
+
+import Model.Usuario;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author pedro
+ */
+public class UsuarioBanco {
+    private final File path = new File(System.getProperty("user.dir") + "/src/main/java/BDO/Arquivo/Usuario.txt");
+    
+    public List retornaUsuarios(){
+        List<Usuario> usuarios = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+            String line = br.readLine();
+            line = br.readLine();
+            while(line != null){
+                String [] vet = line.split(",");
+                int id = Integer.parseInt(vet[0]);
+                String username = vet[1];
+                String senha = vet[2];
+                usuarios.add(new Usuario(id, username, senha));
+            }
+        }catch(IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        return usuarios;
+    }
+}
